15f7bdcf471478cf98b1af78a48aa94f9b2b39c2
allformight <normalpunch@csie.io>
2016-07-01 11:08:16 -0700

    Update __init__.py

--- flaskproj/__init__.py
+++ flaskproj/__init__.py
@@ -1,12 +1,15 @@
 from __future__ import unicode_literals
 from flask import Flask,render_template, request,send_file
-
+import threading
 import getMusic,os.path
+
 
 
 app = Flask(__name__)
 filename = ''
 path =''
+download_link = ''
+flag = 0   
 
 @app.route('/')
 def index(): 
@@ -15,29 +18,35 @@ def index():
 # @app.route('/')
 # def index():
 #     return render_template('test.html')
+
+@app.route('/loading', methods=['POST'])
+def loading():
+    global download_link,flag,path
+
+    download_link = request.form.get('textField','all')
+    checkYtube = "https://www.youtube.com" # check youtube url
+    if (download_link.startswith(checkYtube)): 
+      
+
+        print('dddd')
+        flag=1      
+        return render_template('loading.html')
+        
+     
+    else :
+        flag=0
+        return render_template('errorlink.html')
+        
+   
+
  
-@app.route('/download', methods=['POST'])
+@app.route('/download')
 def download():
     global path
-    download_link = request.form.get('textField','all')    
-    # print(download_link)
-    checkYtube = "https://www.youtube.com" # check youtube url
-    if (download_link.startswith(checkYtube)):
-        # return render_template('pending.html')
-        # print(getMusic.filename)
-        print(path)
-        getMusic.getMusic(download_link)
-        # if getMusic.path_flag == 2 : 
-        
-        path = getMusic.filename
-        #path = unicode(path)
-        print(path)
-        #path = path.encode('latin-1')
-        #path = path.encode('utf-8')
-       
+    if(flag):      
+        getMusic.getMusic(download_link) 
+        path = getMusic.filename  
         return render_template('download.html',link = '/savefile')
-        # else :
-        #     return render_template('download.html',link = '/savefile')
     else :
         return render_template('errorlink.html')
 
@@ -51,4 +60,4 @@ def file_downloads():
 
 
 if __name__=="__main__":
-    app.run(host='140.123.103.172',port=8888)
+    app.run(host='127.0.0.1',port=8888)
